4c97bc6b911b5656c327b834fdf6fab91e635da8
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-03-02 20:49:41 -0300

    Added lib nodemailer, and created config/mail.js

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '5f3dd44ad6b87f',
+        pass: '5a4c2be0ea8f0f',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
