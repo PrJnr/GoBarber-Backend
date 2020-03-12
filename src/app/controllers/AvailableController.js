46b0dc91376e8369ab6e63a4978899e5f553fbf4
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-03-11 22:07:23 -0300

    Updated AppointmentController, added the logic

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,6 +1,31 @@
+import { Op } from 'sequelize';
+import { startOfDay, endOfDay } from ' date-fns';
+import Appointment from '../models/Appointment';
+
 class AvailableController {
     async index(req, res) {
-        return res.json();
+        const { date } = req.query;
+
+        if (!date) {
+            return res.status(400).json({ error: 'Invalid date' });
+        }
+
+        const searchDate = Number(date);
+
+        const appointments = await Appointment.findAll({
+            where: {
+                provider_id: req.params.providerId,
+                canceled_at: null,
+                date: {
+                    [Op.between]: [
+                        startOfDay(searchDate),
+                        endOfDay(searchDate),
+                    ],
+                },
+            },
+        });
+
+        return res.json(appointments);
     }
 }
 
